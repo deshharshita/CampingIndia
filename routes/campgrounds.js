586190b7a5fbc7a9f00b93f201ea908d9b1bcb38
deshharshita <64245643+deshharshita@users.noreply.github.com>
2021-09-03 18:38:50 +0530

    added price in create route

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -25,20 +25,21 @@ router.get('/new', middleware.isLoggedIn, function(req,res){
 router.post("/", middleware.isLoggedIn, function(req, res){
     // get data from form and add to campgrounds array
     var name = req.body.name;
+    var price = req.body.price;
     var image = req.body.image;
     var desc = req.body.description;
     var author = {
         id: req.user._id,
         username: req.user.username
     }
-    var newCampground = {name: name, image: image, description: desc, author:author}
+    var newCampground = {name: name, price: price, image: image, description: desc, author:author}
     // Create a new campground and save to DB
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
             console.log(err);
         } else {
             //redirect back to campgrounds page
-            console.log(newlyCreated);
+            // console.log(newlyCreated);
             res.redirect("/campgrounds");
         }
     });
